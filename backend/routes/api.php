49ac35b950f6f11a6a6ab3e563e2a8783169296f
krc-coding <kasper@ulvedalen.dk>
2024-10-13 21:38:29 +0200

    Added api routes for car controller methods

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\CarController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/register/car', [CarController::class, 'registerCar']);
+    Route::put('/update/car/status', [CarController::class, 'updateCarStatus']);
+});
