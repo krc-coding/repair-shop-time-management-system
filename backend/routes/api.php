835093a1143b8ed82ce132c4c86ee718b8214987
krc-coding <kasper@ulvedalen.dk>
2024-11-26 19:37:39 +0100

    Added routes for timers api endpoints

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,16 @@
 <?php
 
-use App\Http\Controllers\CarController;
+use App\Http\Controllers\Api\CarController;
+use App\Http\Controllers\Api\TimerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/cars', [CarController::class, 'list']);
+Route::get('/timers', [TimerController::class, 'list']);
+Route::post('/timer/start/{type}/{car}', [TimerController::class, 'createTimerForCar']);
+Route::post('/timer/stop/{type}/{car}', [TimerController::class, 'stopTimer']);
+Route::get('/timer/total/{type}/{car}', [TimerController::class, 'getTimerTotal']);
